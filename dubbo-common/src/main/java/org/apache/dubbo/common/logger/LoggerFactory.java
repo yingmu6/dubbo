2910459be351c5976786b53a8a20b2451d2cbd71
chenshengyong <chensy@tuya.com>
2022-07-07 22:45:52 +0800

    Logger日志相关阅读

--- dubbo-common/src/main/java/org/apache/dubbo/common/logger/LoggerFactory.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/logger/LoggerFactory.java
@@ -41,7 +41,7 @@ public class LoggerFactory { //日志工厂
 
     // search common-used logging frameworks
     static { // 静态方法
-        String logger = System.getProperty("dubbo.application.logger", ""); //获取系统属性中日志相关的值
+        String logger = System.getProperty("dubbo.application.logger", ""); // 获取系统属性中日志相关的值
         switch (logger) { //根据系统属性设置的值，选择日志处理适配器
             case "slf4j":
                 setLoggerAdapter(new Slf4jLoggerAdapter());
