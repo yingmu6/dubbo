0f1f5cacad4f3731ad19dfd931ad734c7e6785ee
chenshengyong <chensy@tuya.com>
2022-08-08 23:29:49 +0800

    Dispatcher阅读

--- dubbo-remoting/dubbo-remoting-api/src/main/java/org/apache/dubbo/remoting/Dispatcher.java
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/org/apache/dubbo/remoting/Dispatcher.java
@@ -25,7 +25,7 @@ import org.apache.dubbo.remoting.transport.dispatcher.all.AllDispatcher;
  * ChannelHandlerWrapper (SPI, Singleton, ThreadSafe)
  */
 @SPI(AllDispatcher.NAME)
-public interface Dispatcher {
+public interface Dispatcher { //事件派发
 
     /**
      * dispatch the message to threadpool.
